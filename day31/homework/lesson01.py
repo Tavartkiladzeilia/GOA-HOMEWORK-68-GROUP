7add545126c979409d69750c0daf362f01d60a47
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-11-30 15:11:03 +0400

    second commit

---
+++ day31/homework/lesson01.py
@@ -0,0 +1,50 @@
+# პარამეტრი - ეს ცვლადი არის რომელიც ფუნქციის დეფინიციის დროს შეიქმნება და გამოიყენება იმისთვის  რომ ფუნქციაში გადაეცემა მონაცემები
+# არგუმენტი - და არგუმენტი ეს არის ის მონაცემები  რომლებიც ფუნქციას გადაეცემა ფუნქციის გამოძახების დროს
+
+# es aris 1 davaleba
+
+# meore ver mivxvdi
+
+def sum_numbers(numbers):
+    return sum(numbers)
+
+# es aris 3 davaleba
+
+
+def filter_names(names):
+    return [name for name in names if len(name) >= 5]
+
+# es aris 4 davaleba
+
+
+def double_numbers(numbers):
+    return [num * 2 for num in numbers]
+
+# es aris 5 davaleba
+
+
+def reverse_list(numbers):
+    return numbers[::-1]
+
+# es aris 6 davaleba
+
+
+def is_palindrome(word):
+    return word.lower() == word.lower()[::-1]
+
+# es aris 7 davaleba
+
+
+def reverse_string(text):
+    return text[::-1]
+
+# es aris 8 daavaleba
+
+
+def ilia_negative(number):
+    return -abs(number)
+
+# es aris 9 davaleba
+
+
+# meate ver mivxvdi.
