83055a6b6cd2c89750187cc040d2102ad03891ec
Tavartkiladzeilia <tavartkiladzeilia0407@gmail.com>
2025-11-30 17:21:43 +0400

    second commit

---
+++ day32/homework/lesson01.py
@@ -0,0 +1,53 @@
+# 1.
+
+# 1)გაანგარიშეთ ნებისმიერი ორი რიცხვის ჯამი return ფუნქციით:
+
+def sum_numbers(a, b):
+    return a + b
+
+print(sum_numbers(3, 5))  
+
+
+
+# 2) გადატეხეთ ტექსტი სიტყვებად:
+
+def split_text(text):
+    return text.split()
+
+print(split_text("Hello world, how are you?"))
+# Output: ['Hello', 'world,', 'how', 'are', 'you?']
+
+
+# 3) გამოიტანეთ ყველაზე დიდი რიცხვი:
+
+def max_number(numbers):
+    return max(numbers)
+
+print(max_number([10, 20, 30, 40, 50]))
+# Output: 50
+
+
+# 4) გააკეთედ ისე რომ ტერმინალში თუ დაწერ სიტყვას მაშინ მერე მეორეჯერ დაწერ და ორივე სიტყვას შეატყუპებ 1 სფეისით:
+
+x = input()
+y = input()
+
+
+# 5) დაწერე if, elif და else-ის მაგალითი:
+
+age = int( input("enter your age: "))
+
+if age > 18:
+    print("you are adult!")
+
+elif age ==  18:
+    print("you are became adult!")
+
+else:
+    print("you are child and bye, bye!!!")
+
+    
+
+# 2.
+
+# ok
